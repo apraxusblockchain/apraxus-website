b449a97b52d7056e16a4a141a95c70ee0f56a55f
Apraxus <apraxusblockchain@gmail.com>
2026-09-20 13:27:21 +0530

    fix: remove public SDK API key exposure

--- lib/sdk/config.ts
+++ lib/sdk/config.ts
@@ -1,5 +1,2 @@
 export const APRAXUS_API_VERSION = "v1";
 export const APRAXUS_NETWORK = "arbitrum-sepolia";
-
-export const getApraxusApiKey = () =>
-  process.env.NEXT_PUBLIC_APRAXUS_API_KEY ?? "";
--- lib/sdk/index.ts
+++ lib/sdk/index.ts
@@ -6,5 +6,4 @@ export { createApraxusClient } from "./client";
 export {
   APRAXUS_API_VERSION,
   APRAXUS_NETWORK,
-  getApraxusApiKey,
 } from "./config";
